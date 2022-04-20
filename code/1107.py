79fa7f68eb433e0d3c00c045c9d7322f760bc2ae
hosung-222 <62132755+hosung-222@users.noreply.github.com>
2022-04-20 22:07:01 +0900

    upload

--- code/1107.py
+++ code/1107.py
@@ -1,18 +1,19 @@
 #리모컨
 target = int(input())
-ans = abs(100-target)
+ans = abs(100-target) # + or -로만 이동할때 (최대값)
 m = int(input())
-if m:
+if m: #고장난게 있을경우에만 input 실행
     b = set(input().split())
 else:
     b = set()
 
 for num in range(1000001):
     for n in str(num):
-        if n in b:
+        if n in b: #눌러서 만들수 없는 숫자 포함이면 종료
             break
-    
-    ans = min(ans, len(str(num))+abs(num - target))
+    else:
+        #기존값과 버튼을 누르고 + - 버튼으로 간 횟수중 최솟값
+        ans = min(ans, len(str(num))+abs(num - target))
 
 print(ans)
 
---
+++ code/1260.py
@@ -0,0 +1,51 @@
+#DFS 와 BFS
+from collections import deque
+import sys
+input = sys.stdin.readline
+def dfs(v):
+    print(v, end = ' ')
+    visit[v] = True
+    for i in graph[v]:
+        if visit[i] == False:
+            dfs(i)
+
+# def bfs(n):
+#     visit[n] == True
+#     queue = deque([n])
+#     while queue:
+#         v = queue.popleft()
+#         print(v, end =' ')
+#         for i in graph[v]:
+#             if not visit[i]:
+#                 queue.append(i)
+#                 visit[i] = True
+
+def bfs(n):
+    visit[n] = True
+    queue = deque([n])
+    while queue:
+        v = queue.popleft()
+        print(v, end= ' ')
+        for i in graph[v]:
+            if not visit[i]:
+                queue.append(i)
+                visit[i] = True
+
+
+n, m , v = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+
+for i in range(m):
+    v1,v2 = map(int,input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+print(graph)
+for i in range(1, n+1):
+    graph[i].sort()
+
+print(graph)
+visit = [False] * (n+1)
+dfs(v)
+print()
+visit = [False] * (n+1)
+bfs(v)
